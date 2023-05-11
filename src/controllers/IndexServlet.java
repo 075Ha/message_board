7b8fa05f9628ff772e1ab0cb5318a443afc430ac
User <User@DESKTOP-T8QQU98>
2023-05-11 10:50:51 +0900

    「バリデーションを追加」

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -31,14 +32,36 @@ public class IndexServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();//get datat from db into List
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (NumberFormatException e) {
+        }
+
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
+
+        // 全件数を取得
+        long messages_count = (long) em.createNamedQuery("getMessagesCount", Long.class)
+                .getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);//set list varibale messages to messages
+        request.setAttribute("messages_count", messages_count); // 全件数
+        request.setAttribute("page", page);
+
+        if (request.getSession().getAttribute("flush") != null) {//get attribute from its session
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));// set attribute to new session
+            request.getSession().removeAttribute("flush");//remove attribute from its session
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");//set the address
         rd.forward(request, response);// sending request using rd
